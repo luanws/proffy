a93ec0a2c888d82624536ceb1d8e353bb70acfe1
luanws <luan.w.silveira@gmail.com>
2020-08-04 19:08:04 -0300

    criação de um controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,71 +1,10 @@
 import express from 'express'
-import db from './database/connection'
-import convertHourToMinutes from './utils/convert-hour-to-minutes'
+import ClassesController from './controllers/classes-controller'
 
 const routes = express.Router()
 
-interface ScheduleItem {
-    week_day: number
-    from: string
-    to: string
-}
+const classesControllers = new ClassesController()
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Proffy' })
-})
-
-routes.post('/classes', async (request, response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body
-
-    const trx = await db.transaction()
-
-    try {
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio
-        })
-
-        const user_id = insertedUsersIds[0]
-
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id
-        })
-
-        const class_id = insertedClassesIds[0]
-
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to),
-            }
-        })
-
-        await trx('class_schedule').insert(classSchedule)
-
-        await trx.commit()
-
-        return response.status(201).send()
-    } catch (error) {
-        await trx.rollback()
-
-        return response.status(400).json({
-            error: 'Unexpected error while creating new class'
-        })
-    }
-})
+routes.post('/classes', classesControllers.create)
 
 export default routes
