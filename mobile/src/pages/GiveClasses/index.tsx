f133a66eace340432a2d011c1ce914b85f67bf28
luanws <luan.w.silveira@gmail.com>
2020-08-07 16:19:38 -0300

    navegação para give classes

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+// import { Container } from './styles'
+
+const GiveClasses: React.FC = () => {
+  return (
+    <View>
+      <Text>Give classes</Text>
+    </View>
+  )
+}
+
+export default GiveClasses
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -5,8 +5,14 @@ import landingImg from '../../assets/images/landing.png'
 import studyIcon from '../../assets/images/icons/study.png'
 import giveClassesIcon from '../../assets/images/icons/give-classes.png'
 import heartIconIcon from '../../assets/images/icons/heart.png'
+import { useNavigation } from '@react-navigation/native'
+import { RectButton } from 'react-native-gesture-handler'
 
 const Landing: React.FC = () => {
+  const { navigate } = useNavigation()
+
+  const navigateToGiveClassesPage = () => navigate('GiveClasses')
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.subContainer}>
@@ -25,19 +31,22 @@ const Landing: React.FC = () => {
         </Text>
 
         <View style={styles.buttonsContainer}>
-          <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+          <RectButton style={[styles.button, styles.buttonPrimary]}>
             <Image
               source={studyIcon}
             />
             <Text style={styles.buttonText}>Estudar</Text>
-          </TouchableOpacity>
+          </RectButton>
 
-          <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+          <RectButton
+            onPress={navigateToGiveClassesPage}
+            style={[styles.button, styles.buttonSecondary]}
+          >
             <Image
               source={giveClassesIcon}
             />
             <Text style={styles.buttonText}>Dar aulas</Text>
-          </TouchableOpacity>
+          </RectButton>
         </View>
 
         <Text style={styles.totalConnections}>
