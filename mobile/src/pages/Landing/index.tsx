263ebf93e1bfeea2ebb53ad7b0b7a80340245e57
luanws <luan.w.silveira@gmail.com>
2020-08-07 15:36:35 -0300

    fonts

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { View } from 'react-native'
-
-// import { Container } from './styles'
+import { View, Image, Text } from 'react-native'
+import styles from './styles'
+import landingImg from '../../assets/images/landing.png'
 
 const Landing: React.FC = () => {
-  return <View />
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.banner}
+        source={landingImg}
+      />
+      <Text
+        style={styles.title}
+      >
+        Seja bem-vindo, {'\n'}
+        <Text
+          style={styles.titleBold}
+        >O que deseja fazer?</Text>
+      </Text>
+    </View>
+  )
 }
 
 export default Landing
--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -1,7 +1,26 @@
 import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
-
+    container: {
+        backgroundColor: '#8257e5',
+        flex: 1,
+        justifyContent: 'center',
+        padding: 40
+    },
+    banner: {
+        width: '100%',
+        resizeMode: 'contain',
+    },
+    title: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#fff',
+        fontSize: 20,
+        lineHeight: 30,
+        marginTop: 80,
+    },
+    titleBold: {
+        fontFamily: 'Poppins_600SemiBold',
+    }
 })
 
 export default styles
