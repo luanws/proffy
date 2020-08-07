bdc288091850f54dbbe3eedd590809fb2800e490
luanws <luan.w.silveira@gmail.com>
2020-08-07 17:30:18 -0300

    Tela de dar aulas

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground } from 'react-native'
+import giveClassesBackgroundImage from '../../assets/images/give-classes-background.png'
 
-// import { Container } from './styles'
+import styles from './styles'
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
 
 const GiveClasses: React.FC = () => {
+  const { goBack } = useNavigation()
+
+  const navigateBack = () => goBack()
+
   return (
-    <View>
-      <Text>Give classes</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        style={styles.content}
+        source={giveClassesBackgroundImage}
+      >
+        <Text
+          style={styles.title}
+        >Quer ser um Proffy?</Text>
+        <Text
+          style={styles.description}
+        >Para começar, você precisa se cadastrar como professor na nossa plataforma web.</Text>
+      </ImageBackground>
+      <RectButton
+        onPress={navigateBack}
+        style={styles.okButton}
+      >
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
     </View>
   )
 }
