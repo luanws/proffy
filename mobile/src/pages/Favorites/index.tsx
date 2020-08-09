d13cdc642941eb80683490f7bb2430ac5b92ac9a
luanws <luan.w.silveira@gmail.com>
2020-08-09 15:10:17 -0300

    favorites

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 import styles from './styles'
+import PageHeader from '../../components/PageHeader'
 
 const Favorites: React.FC = () => {
   return (
     <View style={styles.container}>
-      
+      <PageHeader title="Meus Proffys favoritos" />
     </View>
   )
 }
