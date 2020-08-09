28d0468954f189160b25e90d84967b88e1ee10ca
luanws <luan.w.silveira@gmail.com>
2020-08-09 15:13:47 -0300

    Update index.tsx

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { View, Image, Text } from 'react-native'
 import styles from './styles'
 import { BorderlessButton } from 'react-native-gesture-handler'
 import backIcon from '../../assets/images/icons/back.png'
 import logoImg from '../../assets/images/logo.png'
+import { useNavigation } from '@react-navigation/native'
 
 interface Props {
   title: string
@@ -12,8 +13,14 @@ interface Props {
 const PageHeader: React.FC<Props> = (props) => {
   const { title } = props
 
-  function handleGoBack() {
+  const navigation = useNavigation()
 
+  useCallback(() => {
+    
+  }, [])
+
+  function handleGoBack() {
+    navigation.navigate('Landing')
   }
 
   return (
